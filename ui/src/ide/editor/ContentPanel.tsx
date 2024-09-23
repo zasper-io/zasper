8ebff76512530671fbd87e9c0b23d4f8395df833
Prasun Anand <prasunanand.bitsp@gmail.com>
2024-09-23 13:32:43 +0530

    remove dead code

--- ui/src/ide/editor/ContentPanel.tsx
+++ ui/src/ide/editor/ContentPanel.tsx
@@ -2,29 +2,6 @@ import React, { useEffect, useState } from 'react'
 import Editor from './Editor'
 
 export default function ContentPanel (props) {
-  const handleKeyPress = () => {
-    alert('Detected Shift+Enter')
-    // if (e.key === 'Enter' && e.shiftKey) {
-
-    //     return CodeMirror.Pass;
-    // }
-  }
-
-  const onChange = React.useCallback((value, viewUpdate) => {
-    console.log('value:', value)
-  }, [])
-
-  const tabToggle = () => {
-  }
-
-  const closeTab = () => {
-    alert('Tab close')
-  }
-
-  useEffect(() => {
-    // listContents();
-  }, [])
-
   return (
     <div className='tabContent'>
       {Object.keys(props.tabs).map((key, index) => (
