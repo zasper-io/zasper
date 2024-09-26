c9774427b07be10e2ec26c91c3374f27b7465739
Prasun Anand <prasunanand.bitsp@gmail.com>
2024-09-26 12:20:27 +0530

    Server starts like a charm ⚡

--- ui/public/electron.js
+++ ui/public/electron.js
@@ -3,40 +3,56 @@ const path = require('path')
 const url = require('url')
 const os = require('os');
 const { exec, execFile } = require('child_process')
+const http = require('http');
 
 var log  = require('electron-log')
 
 // Optional, initialize the logger for any renderer process
 log.transports.file.level = 'silly'
 
+const isApiServerReady = (port, callback) => {
+  const options = {
+      hostname: 'localhost',
+      port: port,
+      path: '/api/health', // Modify to your actual health check endpoint
+      method: 'GET',
+  };
 
-// Create the native browser window.
-function createWindow () {
-  execFile(path.join(__dirname, 'zasper'), { cwd: os.homedir() }, (error, stdout, stderr) => {
-    if (error) {
-      log.error(`Error starting gobackend: ${error.message}`)
-      return
-    }
-    if (stderr) {
-      log.error(`stderr: ${stderr}`)
-      return
-    }
-    log.info(`stdout: ${stdout}`)
-  })
+  const req = http.request(options, (res) => {
+    // callback(res.statusCode === 200);
+    callback(true);
+  });
 
-  const mainWindow = new BrowserWindow({
-    width: 1350,
-    height: 900,
-    show: false,
-    // Set the path of an additional "preload" script that can be used to
-    // communicate between the node-land and the browser-land.
+  req.on('error', () => {
+      callback(false);
+  });
 
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    },
-  })
+  req.end();
+};
 
-  // and load the index.html of the app.
+const startApiServer = () => {
+  const apiProcess =  execFile(path.join(__dirname, 'zasper'), { cwd: os.homedir() });
+
+  apiProcess.stdout.on('data', (data) => {
+      console.log(`API Server: ${data}`);
+  });
+
+  apiProcess.stderr.on('data', (data) => {
+      console.error(`API Server Error: ${data}`);
+  });
+
+  apiProcess.on('close', (code) => {
+      console.log(`API Server exited with code ${code}`);
+  });
+
+  return apiProcess;
+};
+
+
+const startApp = () => {
+  const apiPort = 8888; // Replace with your actual API port
+  const apiProcess = startApiServer();
+
   var loaderScreen = new BrowserWindow({ 
     width: 700, 
     height: 400, 
@@ -47,12 +63,35 @@ function createWindow () {
   
   loaderScreen.loadFile(path.join(__dirname, 'loadscreen.html'));
   loaderScreen.center();
-  setTimeout(function () {
-    loaderScreen.close();
-    mainWindow.center();
-    mainWindow.show();
-  }, 5000);
 
+  const checkApi = () => {
+      isApiServerReady(apiPort, (isReady) => {
+          if (isReady) {
+            loaderScreen.close();
+            createWindow();
+          } else {
+            setTimeout(checkApi, 1000);
+          }
+      });
+  };
+
+  checkApi();
+}
+
+
+
+// Create the native browser window.
+function createWindow () {
+
+  const mainWindow = new BrowserWindow({
+    width: 1350,
+    height: 900,
+    center: true,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    },
+  })
+ 
   const appURL = app.isPackaged
     ? url.format({
       pathname: path.join(__dirname, 'index.html'),
@@ -62,9 +101,10 @@ function createWindow () {
     : 'http://localhost:3000'
   mainWindow.loadURL(appURL)
 
+
+
   // Automatically open Chrome's DevTools in development mode.
   if (!app.isPackaged) {
-    // loaderScreen.webContents.openDevTools()
     mainWindow.webContents.openDevTools()
   }
 }
@@ -80,14 +120,14 @@ app.whenReady().then(() => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
+      startApp()
       log.info("Window created!")
     }
 
     
   })
 
-  createWindow()
+  startApp()
 })
 
 ipcMain.handle('dialog:openDirectory', async () => {
