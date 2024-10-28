e0b040563f6882bdc6fc7185d2fdf6925dadffb1
Prasun Anand <prasunanand.bitsp@gmail.com>
2024-10-28 12:58:07 +0530

    Add notebook app interactions

--- ui/src/ide/editor/NotebookEditor.tsx
+++ ui/src/ide/editor/NotebookEditor.tsx
@@ -7,6 +7,7 @@ import { toast, ToastContainer } from 'react-toastify'
 import { v4 as uuidv4 } from 'uuid'
 import Markdown from 'react-markdown'
 import rehypeRaw from 'rehype-raw'
+import { keymap } from '@codemirror/view'
 import 'react-toastify/dist/ReactToastify.css';
 
 import './NotebookEditor.scss'
@@ -21,10 +22,42 @@ export default function NotebookEditor (props) {
     execution_count: number
     source: string
   }
-  interface IFileContents {
-    cells: ICell[]
+  interface INotebookModel {
+    cells: Array<ICell>
+    nbformat: number
+    nbformat_minor: number
+    metadata: INotebookMetadata
   }
-  const [fileContents, setFileContents] = useState<IFileContents>({ cells: [] })
+
+
+  interface INotebookMetadata {
+    kernelspec?: IKernelspecMetadata;
+    language_info?: ILanguageInfoMetadata;
+    orig_nbformat?: number;
+  }
+
+  interface IKernelspecMetadata{
+    name: string;
+    display_name: string;
+  }
+
+  interface ILanguageInfoMetadata {
+    name: string;
+    codemirror_mode?: string;
+    file_extension?: string;
+    mimetype?: string;
+    pygments_lexer?: string;
+  }
+
+  type CellType = 'code' | 'markdown' | 'raw' | string;
+  
+
+  const [notebook, setNotebook] = useState<INotebookModel>({ 
+                                                    cells: [], 
+                                                    nbformat:0, 
+                                                    nbformat_minor:0,
+                                                    metadata: {}
+                                                  })
   const [blankCell, setBlankCell] = useState<ICell>({ execution_count: 0, source: '' })
 
   const [client, setClient] = useState<IClient>({ send: () => { } })
@@ -51,17 +84,18 @@ export default function NotebookEditor (props) {
       })
     })
     const resJson = await res.json()
-    setFileContents(resJson.content)
-    // console.log(resJson['content']);
+    setNotebook(resJson.content)
+    console.log(resJson['content']);
   }
 
   useEffect(() => {
     if (props.data.load_required == true) {
       FetchFileData(props.data.path)
+      // startASession(props.name, props.path, props.type)
+      // listKernels();
+      // listAllSessions();
     }
-    // startASession()
-    // listKernels();
-    // listAllSessions();
+   
   }, [])
 
   const generateOutput = (data) => {
@@ -292,22 +326,91 @@ export default function NotebookEditor (props) {
 
   const saveFile = async () => {
     const path = 'demo.ipynb'
-    console.log(fileContents)
+    console.log(notebook)
     alert('Saving file')
     const res = await fetch('http://localhost:8888/api/contents/demo.ipynb', {
       method: 'PUT',
       body: JSON.stringify({
-        content: JSON.stringify(fileContents),
+        content: JSON.stringify(notebook),
         type: 'file',
         format: 'text'
       })
     })
   }
 
+  const addCellUp = async () =>{
+    console.log("add cell Up");
+    notebook.cells.push({
+      execution_count: 0,
+      source: ""})
+  }
+
+  const addCellDown = async () =>{
+    console.log("add cell Down");
+    notebook.cells.push({
+      execution_count: 0,
+      source: ""})
+  }
+
+  const deleteCell = async (index: number) =>{
+    console.log("delete cell at index => " + index)
+    notebook.cells.pop()
+  }
+
+  const nextCell = async (index: number) =>{
+    console.log("moving to next index => " + (index + 1 ))
+  }
+
+  const prevCell = async (index: number) =>{
+    console.log("moving to prev index => " + (index - 1 ))
+  }
+
+
+  // Notebook level
+
+  let index=0
+
+  const saveNotebook =  () => {
+    console.log("saving notebook!")
+  }
+  const addCell =  () => {
+    console.log("add cell at index => " + index)
+  }
+  const cutCell =  () => {
+    console.log("cut cell at index => " + index)
+  }
+  const copyCell =  () => {
+    console.log("copy cell at index => " + index)
+  }
+  const pasteCell =  () => {
+    console.log("Paste cell at index => " + index)
+  }
+
+  const stopKernel =  () => {
+    console.log("Stop Kernel")
+  }
+  const restartKernel =  () => {
+    console.log("Restart kernel")
+  }
+  const reExecuteNotebook =  () => {
+    console.log("ReExecute Notebook")
+  }
+
+
+
   return (
     <div className='tab-content'>
       <div className={props.data.display} id='profile' role='tabpanel' aria-labelledby='profile-tab'>
-      <NbButtons/>
+      <NbButtons saveNotebook={saveNotebook}
+                addCell={addCell}
+                cutCell={cutCell}
+                copyCell={copyCell}
+                pasteCell={pasteCell}
+                submitCell={submitCell}
+                stopKernel={stopKernel}
+                restartKernel={restartKernel}
+                reExecuteNotebook={reExecuteNotebook}
+      />
         
       {debugMode && <div>
         <button type='button' onClick={saveFile}>Save file</button>
@@ -319,8 +422,18 @@ export default function NotebookEditor (props) {
       }
       <div className='editor-body'>
         {
-          fileContents.cells.map((cell, index) =>
-            <Cell key={index} cell={cell} generateOutput={generateOutput} submitCell={submitCell} />
+          notebook.cells.map((cell, index) =>
+            <Cell key={index} 
+                  index={index} 
+                  cell={cell} 
+                  generateOutput={generateOutput} 
+                  submitCell={submitCell} 
+                  addCellUp={addCellUp} 
+                  addCellDown={addCellDown} 
+                  prevCell={prevCell}
+                  nextCell={nextCell}
+                  deleteCell={deleteCell}
+            />
           )
         }
       </div>
@@ -329,18 +442,23 @@ export default function NotebookEditor (props) {
   )
 }
 
-function NbButtons(){
+function NbButtons(props){
   return (
     <div className='text-editor-tool'>
-      <button type='button' className='editor-button'><i className='fas fa-save' /></button>
-      <button type='button' className='editor-button'><i className='fas fa-cut' /></button>
-      <button type='button' className='editor-button'><i className='fas fa-copy' /></button>
-      <button type='button' className='editor-button'><i className='fas fa-plus' /></button>
-      <button type='button' className='editor-button'><i className='fas fa-check-square' /></button>
-      <button type='button' className='editor-button'><i className='fas fa-play' /></button>
-      <button type='button' className='editor-button'><i className='fas fa-square' /></button>
-      <button type='button' className='editor-button'><i className='fas fa-redo' /></button>
-      <button type='button' className='editor-button'><i className='fas fa-forward' /></button>
+      <button type='button' className='editor-button' onClick={() => props.saveNotebook()}><i className='fas fa-save' /></button>
+      <button type='button' className='editor-button' onClick={() => props.addCell(props.index)}><i className='fas fa-plus' /></button>
+      <button type='button' className='editor-button' onClick={() => props.cutCell(props.index)}><i className='fas fa-cut' /></button>
+      <button type='button' className='editor-button' onClick={() => props.copyCell(props.index)}><i className='fas fa-copy' /></button>
+      <button type='button' className='editor-button' onClick={() => props.pasteCell(props.index)}><i className='fas fa-paste' /></button>
+      <button type='button' className='editor-button' onClick={() => props.submitCell(props.index)}><i className='fas fa-play' /></button>
+      <button type='button' className='editor-button' onClick={() => props.stopKernel()}><i className='fas fa-square' /></button>
+      <button type='button' className='editor-button' onClick={() => props.restartKernel()}><i className='fas fa-redo' /></button>
+      <button type='button' className='editor-button' onClick={() => props.reExecuteNotebook()}><i className='fas fa-forward' /></button>
+      <select className="form-select editor-select" aria-label="Default select example">
+        <option selected>Code</option>
+        <option value="1">Markdown</option>
+        <option value="2">Raw</option>
+      </select>
       
       <div className='ms-auto'>Python [conda env:default]*</div>
       <ToastContainer />
@@ -352,13 +470,12 @@ function CellButtons(props){
   return (
     <div className='cellOptions'>
       <button type='button' className='editor-button' onClick={() => props.submitCell(props.code)}><i className='fas fa-play' /></button>
-      <button type='button' className='editor-button'><i className='fas fa-square' /></button>
-      <button type='button' className='editor-button'><i className='fas fa-redo' /></button>
-      <button type='button' className='editor-button'><i className='fas fa-cut' /></button>
-      <button type='button' className='editor-button'><i className='fas fa-copy' /></button>
-      <button type='button' className='editor-button'><i className='fas fa-plus' /></button>
-      <button type='button' className='editor-button'><i className='fas fa-check-square' /></button>
-      <button type='button' className='editor-button'><i className='fas fa-forward' /></button>
+      <button type='button' className='editor-button' onClick={() => props.copyCell(props.index)}><i className='fas fa-copy' /></button>
+      <button type='button' className='editor-button' onClick={() => props.nextCell(props.index)}><i className='fas fa-forward' /></button>
+      <button type='button' className='editor-button' onClick={() => props.prevCell(props.index)}><i className='fas fa-backward' /></button>
+      <button type='button' className='editor-button' onClick={() => props.addCellUp()}><i className='fas fa-plus' /></button>
+      <button type='button' className='editor-button' onClick={() => props.addCellDown()}><i className='fas fa-plus' /></button>
+      <button type='button' className='editor-button' onClick={() => props.deleteCell(props.index)}><i className='fas fa-trash' /></button>
       <ToastContainer />
     </div>
   )
@@ -373,12 +490,32 @@ function Cell (props) {
     )
   }
 
+  const handleCmdEnter = () => {
+    console.log(alert("running the code"))
+
+    return true
+  }
+
+  const customKeymap = keymap.of([
+    {
+      key: 'Mod-s',
+      run: handleCmdEnter
+    }
+  ])
+
   return (
     <div className='single-line'>
 
       <div className='serial-no'>[{cell.execution_count}]:</div>
       <div className='inner-content'>
-      <CellButtons code={fileContents} submitCell={props.submitCell}/>
+      <CellButtons index={props.index} 
+                  code={fileContents} 
+                  submitCell={props.submitCell} 
+                  addCellUp={props.addCellUp} 
+                  addCellDown={props.addCellDown} 
+                  deleteCell={props.deleteCell} 
+                  nextCell={props.nextCell} 
+                  prevCell={props.prevCell}/>
         <CodeMirror
           value={fileContents}
           height='auto'
