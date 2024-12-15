565de4a328ba9a179edf885cb05854300a07db42
Prasun Anand <prasunanand.bitsp@gmail.com>
2024-12-15 22:08:14 +0530

    fix ci

--- spa.go
+++ spa.go
@@ -1,60 +1,42 @@
-//go:build webapp
-// +build webapp
+//go:build !webapp
+// +build !webapp
 
 package main
 
 import (
-	"embed"
-	"io/fs"
 	"net/http"
 	"os"
 	"path/filepath"
 )
 
-//go:embed ui/build/*
-var staticFiles embed.FS
-
 type spaHandler struct {
-	staticFS   embed.FS
 	staticPath string
 	indexPath  string
 }
 
 func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// get the absolute path to prevent directory traversal
-	path, err := filepath.Abs(r.URL.Path)
-	if err != nil {
-		// if we failed to get the absolute path respond with a 400 bad request and stop
-		http.Error(w, err.Error(), http.StatusBadRequest)
+	// Join internally call path.Clean to prevent directory traversal
+	path := filepath.Join(h.staticPath, r.URL.Path)
+
+	// check whether a file exists or is a directory at the given path
+	fi, err := os.Stat(path)
+	if os.IsNotExist(err) || fi.IsDir() {
+		// file does not exist or path is a directory, serve index.html
+		http.ServeFile(w, r, filepath.Join(h.staticPath, h.indexPath))
 		return
 	}
-	// prepend the path with the path to the static directory
-	path = filepath.Join(h.staticPath, path)
 
-	_, err = h.staticFS.Open(path)
-	if os.IsNotExist(err) {
-		// file does not exist, serve index.html
-		index, err := h.staticFS.ReadFile(filepath.Join(h.staticPath, h.indexPath))
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		w.WriteHeader(http.StatusAccepted)
-		w.Write(index)
-		return
-	} else if err != nil {
+	if err != nil {
 		// if we got an error (that wasn't that the file doesn't exist) stating the
 		// file, return a 500 internal server error and stop
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	// get the subdirectory of the static dir
-	statics, err := fs.Sub(h.staticFS, h.staticPath)
-	// otherwise, use http.FileServer to serve the static dir
-	http.FileServer(http.FS(statics)).ServeHTTP(w, r)
+	// otherwise, use http.FileServer to serve the static file
+	http.FileServer(http.Dir(h.staticPath)).ServeHTTP(w, r)
 }
+
 func getSpaHandler() http.Handler {
-	return spaHandler{staticFS: staticFiles, staticPath: "ui/build", indexPath: "index.html"}
+	return spaHandler{staticPath: "./ui/build", indexPath: "index.html"}
 }
