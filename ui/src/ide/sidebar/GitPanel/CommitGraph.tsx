a1d9f739da59d07d92987033de794c54a0d8e3f9
sridhar02 <kattasridhar02@gmail.com>
2025-02-24 23:06:55 +0530

    update commit graph

--- ui/src/ide/sidebar/GitPanel/CommitGraph.tsx
+++ ui/src/ide/sidebar/GitPanel/CommitGraph.tsx
@@ -1,132 +1,84 @@
 import { useEffect, useRef, useState } from "react";
 import { Commit, CommitNode } from "./types";
 import "./GitPanel.scss";
-import * as d3 from "d3";
-import dagreD3 from "dagre-d3";
 
 export const CommitGraph: React.FC<{ data: Commit[] }> = ({ data }) => {
-  const commits = data;
-  console.log(data);
   const [commitNodes, setCommitNodes] = useState<CommitNode[]>([]);
-  const [isHidden, setIsHidden] = useState<boolean>(false);
-  const svgRef = useRef<SVGSVGElement | null>(null);
-  const [hoveredCommit, setHoveredCommit] = useState<Commit | null>(null);
 
   useEffect(() => {
-    // const commitMap: { [key: string]: CommitNode } = {};
-    // data.forEach((commit) => {
-    //   commitMap[commit.hash] = {
-    //     id: commit.hash,
-    //     x: 0,
-    //     y: 0,
-    //     children: [],
-    //     commit: commit,
-    //   };
-    // });
-
-    // // tree (children linked to parents)
-    // data.forEach((commit) => {
-    //   commit.parents.forEach((parentHash) => {
-    //     if (commitMap[parentHash]) {
-    //       commitMap[parentHash].children.push(commitMap[commit.hash]);
-    //     }
-    //   });
-    // });
-
-    const g = new dagreD3.graphlib.Graph().setGraph({});
-
-    commits.forEach((commit, index) => {
-      if (commit) {
-        g.setNode(commit.hash, {
-          label: `${commit.message} \n ${commit.author}`,
-          class: "commit-node",
-        });
-
-        if (index > 0 && commits[index - 1]) {
-          g.setEdge(commits[index - 1].hash, commit.hash, {
-            class: "commit-edge",
-          });
-        }
-      }
+    const commitMap: { [key: string]: CommitNode } = {};
+    data.forEach((commit) => {
+      commitMap[commit.hash] = {
+        id: commit.hash,
+        x: 0,
+        y: 0,
+        children: [],
+        commit: commit,
+      };
     });
 
-    const svg = d3.select(svgRef.current);
-    const inner = svg.select("g");
-    const render = new dagreD3.render();
-    render(inner, g);
+    // tree (children linked to parents)
+    data.forEach((commit) => {
+      commit.parents.forEach((parentHash) => {
+        if (commitMap[parentHash]) {
+          commitMap[parentHash].children.push(commitMap[commit.hash]);
+        }
+      });
+    });
 
-    // Handle Hover
-    // d3.selectAll(".commit-node").on("mouseover", function (event, d) {
-    //   const commit = commits.find((c) => c.hash === d);
-    //   setHoveredCommit(commit || null);
-    // });
+    // Layout the tree using a simple recursive function
+    let yPosition = 100;
+    const layoutTree = (node: CommitNode, x: number) => {
+      node.x = x;
+      node.y = yPosition;
+      yPosition += 150; // Adjust vertical spacing between nodes
 
-    // d3.selectAll(".commit-node").on("mouseout", function () {
-    //   setHoveredCommit(null);
-    // });
+      node.children.forEach((childNode, index) => {
+        layoutTree(childNode, x + (index - node.children.length / 2) * 200); // Adjust horizontal spacing
+      });
+    };
 
-    // // Layout the tree using a simple recursive function
-    // let yPosition = 100;
-    // const layoutTree = (node: CommitNode, x: number) => {
-    //   node.x = x;
-    //   node.y = yPosition;
-    //   yPosition += 150; // Adjust vertical spacing between nodes
+    // Assuming root node is the first commit (no parents)
+    const rootCommit = data.find((commit) => commit.parents.length === 0);
+    if (rootCommit) {
+      layoutTree(commitMap[rootCommit.hash], 400);
+    }
 
-    //   node.children.forEach((childNode, index) => {
-    //     layoutTree(childNode, x + (index - node.children.length / 2) * 200); // Adjust horizontal spacing
-    //   });
-    // };
-
-    // // Assuming root node is the first commit (no parents)
-    // const rootCommit = data.find((commit) => commit.parents.length === 0);
-    // if (rootCommit) {
-    //   layoutTree(commitMap[rootCommit.hash], 400);
-    // }
-
-    // // Set the nodes to state
-    // setCommitNodes(Object.values(commitMap));
+    // Set the nodes to state
+    setCommitNodes(Object.values(commitMap));
   }, [data]);
 
+  const truncateText = (text: string, maxLength: number) => {
+    return text.length > maxLength
+      ? text.substring(0, maxLength) + "..."
+      : text;
+  };
+
   return (
-    <div style={{ height: isHidden ? "0px" : "100%", margin:"0 10px" }}>
+    <>
       <div className="projectName">
-        <div onClick={() => setIsHidden(!isHidden)}>SOURCE CONTROL GRAPH</div>
+        <div>SOURCE CONTROL GRAPH</div>
       </div>
-
-      
-      <svg ref={svgRef} width="100%" height="500px" className="git-graph-svg">
-        <g />
-      </svg>
-
-       {/* Hover Popup */}
-      {hoveredCommit && (
-        <div className="commit-popup">
-          <p><strong>Commit:</strong> {hoveredCommit?.hash}</p>
-          <p><strong>Message:</strong> {hoveredCommit?.message}</p>
-          <p><strong>Author:</strong> {hoveredCommit?.author}</p>
-        </div>
-      )}
-
-      {isHidden && (
-        <div className="git-commit-content">
-          <div className="graph-container">
-            {commitNodes.map((node) => (
-              <div
-                key={node.id}
-                className={`commit-node ${node.commit.branch}`}
-                style={{
-                  left: `${node.x}px`,
-                  top: `${node.y}px`,
-                }}
-              >
-                <span className="commit-message">
-                  {node.commit.message} -- {node.commit.author}
-                </span>
-                {/* <span className="commit-hash">{node.commit.hash}</span> */}
-              </div>
-            ))}
-            {/* Lines to connect parent-child relationships */}
-            {/* {commitNodes.map(node =>
+      <div className="git-commit-content">
+        <div className="graph-container">
+          {commitNodes.map((node, index) => (
+            <div
+              key={node.id}
+              className={`commit-row`}
+              style={{ left: `${node.x}px`, top: `${node.y}px` }}
+            >
+              <div className="commit-circle" />
+              {index < commitNodes.length - 1 && (
+                <div className="commit-line" />
+              )}
+              <span className="commit-text">
+                {truncateText(node.commit.message, 75)} -- {node.commit.author}
+              </span>
+              {/* <span className="commit-hash">{node.commit.hash}</span> */}
+            </div>
+          ))}
+          {/* Lines to connect parent-child relationships */}
+          {/* {commitNodes.map(node =>
           node.children.map(child => (
             <div
               key={`${node.id}-${child.id}`}
@@ -141,9 +93,8 @@ export const CommitGraph: React.FC<{ data: Commit[] }> = ({ data }) => {
               />
             ))
           )} */}
-          </div>
         </div>
-      )}
-    </div>
+      </div>
+    </>
   );
 };
