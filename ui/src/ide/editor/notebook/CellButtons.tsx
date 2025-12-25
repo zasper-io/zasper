7ec6be096f8a265df0d8d19bb14567b9db1f162e
Prasun Anand <prasunanand.bitsp@gmail.com>
2025-12-25 16:58:33 +0530

    Change AddCellUp and AddCellBelow Icon

--- ui/src/ide/editor/notebook/CellButtons.tsx
+++ ui/src/ide/editor/notebook/CellButtons.tsx
@@ -21,10 +21,10 @@ function CellButtons(props) {
           <i className="fas fa-backward" />
         </button>
         <button type="button" className="editor-button" onClick={() => props.addCellUp()}>
-          <i className="fas fa-plus" />
+          <IconAddAbove />
         </button>
         <button type="button" className="editor-button" onClick={() => props.addCellDown()}>
-          <i className="fas fa-plus" />
+          <IconAddBelow />
         </button>
         <button
           type="button"
@@ -37,5 +37,63 @@ function CellButtons(props) {
     </div>
   );
 }
+
+const IconAddAbove = ({ size = 24, color = 'currentColor' }) => {
+  const maskId = 'mask-plus-above';
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <defs>
+        <mask id={maskId}>
+          <rect width="24" height="24" fill="white" />
+          <path d="M6 12H12M9 9V15" stroke="black" strokeWidth="2.5" strokeLinecap="round" />
+        </mask>
+      </defs>
+      <rect x="1" y="7" width="16" height="10" rx="2" fill={color} mask={`url(#${maskId})`} />
+      <path
+        d="M19 11L21.5 8.5L24 11"
+        stroke={color}
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path d="M21.5 8.5V16" stroke={color} strokeWidth="2" strokeLinecap="round" />
+    </svg>
+  );
+};
+
+const IconAddBelow = ({ size = 24, color = 'currentColor' }) => {
+  const maskId = 'mask-plus-below';
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <defs>
+        <mask id={maskId}>
+          <rect width="24" height="24" fill="white" />
+          <path d="M6 12H12M9 9V15" stroke="black" strokeWidth="2.5" strokeLinecap="round" />
+        </mask>
+      </defs>
+      <rect x="1" y="7" width="16" height="10" rx="2" fill={color} mask={`url(#${maskId})`} />
+      <path
+        d="M19 13L21.5 15.5L24 13"
+        stroke={color}
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path d="M21.5 15.5V8" stroke={color} strokeWidth="2" strokeLinecap="round" />
+    </svg>
+  );
+};
 
 export default CellButtons;
